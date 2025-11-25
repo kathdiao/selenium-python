1e4adff2e7d939f8221b52928ea56bbbf76830bf
kathdiao <katherinejoycediao22@gmail.com>
2025-11-25 13:33:36 +0800

    Separate pytest

--- pytest/main.py
+++
@@ -1,5 +0,0 @@
-def get_weather(temp):
-    if temp > 20:
-        return "hot"
-    else:
-        return "cold" 
--- pytest/test_main.py
+++
@@ -1,4 +0,0 @@
-from main import get_weather
-
-def test_get_weather():
-    assert get_weather(21) == "cold"
